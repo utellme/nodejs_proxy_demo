c98f3ac70b5c4b51b8a95546dcaac065fea8f1e9
Dipesh Valia <dvalia@walmartlabs.com>
2015-04-24 12:52:15 -0700

    fixed logfile and x-url-destinaiton

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let path = require('path');
 let http = require("http");
 let fs = require('fs');
 let request = require('request');
+let through = require('through');
 
 let scheme = 'http://';
  let argv = require('yargs')
@@ -14,9 +16,9 @@ console.log(argv);
 
  let port = argv.port || argv.host === '127.0.0.1' ? 8000:80;
 
- let destinationUrl = scheme + argv.host + ':' + port;
+ let destinationUrl = argv.url || scheme + argv.host + ':' + port;
 
- let outputStream = argv.log1 ? fs.createWriteStream(argv.log1) : process.stdout;
+ let outputStream = argv.logfile1 ? fs.createWriteStream(path.join(__dirname, argv.logfile1)) : process.stdout;
 
  //console.log(destinationUrl);
 // console.log(outputStream);
@@ -28,20 +30,23 @@ function onRequest(req, res) {
   
 
 
-  outputStream.write('\ Request received at ' + req.url);
+  outputStream.write('\n\n\n\nEcho Request received at ' + req.url + '\n\n' + JSON.stringify(req.headers));
 
   //outputStream.write('\nRequest received at:  ${req.url}');
-  req.pipe(outputStream);
+  //req.pipe(outputStream);
+ 
 
   //response.writeHead(200, {"Content-Type": "text/plain"});
   for(let header in req.headers){
 
  	res.setHeader(header, req.headers[header]);
+
  }
 
  //res.write("Hello World\n");
 
-  
+   through(req, outputStream, {autoDestroy: false});
+
   req.pipe(res);
   //res.end();
 }
@@ -56,36 +61,45 @@ function onProxyRequest(req, res) {
   
 	//let destinationUrl = '127.0.0.1:8000';
 	
-	//let num = 0;
-	let url;
 
 	///console.log('\nProxying request to ' + destinationUrl + req.url);
-	outputStream.write('\nProxying request to ' + destinationUrl + req.url);
-
-	
-
-   url = destinationUrl + req.url;
+	outputStream.write('\nRequest Proxied to ' + destinationUrl + req.url);
+	through(req, outputStream, {autoDestroy: false})
 
 
-  let options = { 
-  //	method:req.method,
-  	headers: req.headers,
-  	//url: `http://${destinationUrl}${req.url}`
-  	url: url
+   let url = destinationUrl
+
+   //console.log("\nRequest " + req.headers);
+
+  if (req.headers['x-destination-url']) {
+    url = req.headers['x-destination-url']
+    delete req.headers['x-destination-url']
   }
 
-  outputStream.write('\n\n\n' + JSON.stringify(req.headers));
+
+  let options = {
+    headers: req.headers,
+    url: url + req.url
+  }
+
+
+
+  outputStream.write('\n\n' + JSON.stringify(req.headers));
 	  //req.pipe(process.stdout);
 	 // req.pipe(process.stdout);
-  req.pipe(outputStream);
+ // req.pipe(outputStream);
+ through(req, outputStream, {autoDestroy: false});
 
   let downstreamResponse = req.pipe(request(options));
 
-  //outputStream.write(JSON.stringify(downstreamResponse.headers));
+  outputStream.write('\n\n' + JSON.stringify(downstreamResponse.headers));
 
-  downstreamResponse.pipe(outputStream);
-	
+  //downstreamResponse.pipe(outputStream);
+ 
   downstreamResponse.pipe(res);
+  
+  through(downstreamResponse, outputStream, {autoDestroy: false});
+  
 	
 
  /* request(options);
